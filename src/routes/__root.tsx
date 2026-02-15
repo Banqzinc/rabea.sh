cd9270991ce48da5f7803848e8d30d5254569f5b
Rabea Bader <rabea@battata.io>
2026-02-15 22:00:32 +0100

    Add Google Analytics and Microsoft Clarity tracking scripts for production environment

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,6 +2,8 @@ import { HeadContent, Outlet, Scripts, createRootRoute } from '@tanstack/react-r
 import appCss from '../styles.css?url'
 
 const ICON_URL = '/favicon.svg'
+const GA_MEASUREMENT_ID = 'G-XT5RY2JPKY'
+const CLARITY_PROJECT_ID = 'vhxy08rzii'
 
 export const Route = createRootRoute({
   head: () => ({
@@ -28,6 +30,32 @@ function RootComponent() {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
+        {import.meta.env.PROD ? (
+          <>
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${GA_MEASUREMENT_ID}');
+`,
+              }}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "${CLARITY_PROJECT_ID}");
+`,
+              }}
+            />
+          </>
+        ) : null}
         <HeadContent />
       </head>
       <body suppressHydrationWarning>
